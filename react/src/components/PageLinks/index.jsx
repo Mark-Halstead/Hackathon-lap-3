752143801fe5341c83a417df35a482ac8511afd8
Mark Halstead <103516032+Mark-Halstead@users.noreply.github.com>
2023-03-03 10:09:06 +0000

    added exports + imports

---
+++ react/src/components/PageLinks/index.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const PageLinks = () => {
+  return (
+    <div>PageLinks</div>
+  )
+}
+
+export default PageLinks
---
+++ react/src/components/index.jsx
@@ -0,0 +1,3 @@
+export {default as Navbar} from './Navbar'
+export {default as PageLink} from './PageLink'
+export {default as PageLinks} from './PageLinks'
